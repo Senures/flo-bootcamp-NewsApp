5885eb14fc321c0b40e1a595d53af8237a9d5c67
Semanur Eserler <78433748+Senures@users.noreply.github.com>
2023-07-20 00:19:31 +0300

    add loading indicator.

--- Flo-News-App/Login/ForgotPasswordViewController.swift
+++ Flo-News-App/Login/ForgotPasswordViewController.swift
@@ -14,7 +14,7 @@ class ForgotPasswordViewController: UIViewController {
     @IBOutlet weak var email: UITextField!
     override func viewDidLoad() {
         super.viewDidLoad()
-       
+        
         sendMailBtn.applyCornerRadius(5.0)
         
     }
@@ -33,7 +33,7 @@ class ForgotPasswordViewController: UIViewController {
         self.dismiss(animated: true)
     }
     
-    @IBOutlet weak var isLoading: UIActivityIndicatorView!
+    
     func showAlert(message: String) {
         let alert = UIAlertController(title: "Warning", message: message, preferredStyle: .alert)
         alert.addAction(UIAlertAction(title: "OK", style: .default, handler: nil))
@@ -47,14 +47,12 @@ class ForgotPasswordViewController: UIViewController {
         Auth.auth().sendPasswordReset(withEmail:email) { [self] error in
             if let error = error {
                 self.hideActivityIndicator()
-               
                 print("Şifre sıfırlama e-postası gönderilirken bir hata oluştu: \(error.localizedDescription)")
                 self.alertMessage(titleInput:"Error" , messageInput: error.localizedDescription ?? "Error")
                 
             } else {
                 
                 self.hideActivityIndicator()
-                print("Şifre sıfırlama e-postası gönderildi")
                 let alert = UIAlertController(title: "Successful", message: "Password reset email sent", preferredStyle: .alert)
                 let okAction = UIAlertAction(title: "OK", style: .default) { (action) in
                     self.dismiss(animated: true)
